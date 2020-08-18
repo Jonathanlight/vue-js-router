14601f1d795ece3ce6838e1db4d9a9761b809eb3
Jonathan <Jonathan.kablan@gmail.com>
2020-08-18 14:55:45 +0200

    init router vue

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="container-fluid d-flex flex flex-column">
+    <div class="d-flex flex-row align-items-center bg-light mb-5" style="height: 50px;">
+      <a class="px-5" @click="selectedComponent='Comp1'"> Component 1 </a>
+      <a class="px-5" @click="selectedComponent='Comp2'"> Component 2 </a>
+    </div>
+    <div class="m-5">
+      <component :is="selectedComponent"> </component>
+    </div>
+  </div>
+</template>
+
+<script>
+import Comp1 from './components/Comp1'
+import Comp2 from './components/Comp2'
+
+export default {
+  name: 'App',
+  components: {
+    Comp1,
+    Comp2
+  },
+  data() {
+    return {
+      selectedComponent: 'Comp2'
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+a {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Comp1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <div class="card-header text-light" style="background: #e74c3c;"> Component 1 </div>
+    <div class="card-body m-5">
+        <p class="card-text"> C'est un super composant </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comp1',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
